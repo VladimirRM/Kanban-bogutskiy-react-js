2d17689a0cb7b0495a13a8ad272405cbc4dda244
Vladimir RM <shvdrummer@gmail.com>
2023-01-15 21:09:01 +0200

    create select and  default option

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React, { useState } from "react";
 import Column from "./Column";
 
 const initialTasks = [
-  { id: 1, task: "Task-1", status: "todo", priority: 1 },
-  { id: 2, task: "Task-2", status: "progress", priority: 2 },
-  { id: 3, task: "Task-3", status: "review", priority: 3 },
-  { id: 4, task: "Task-4", status: "done", priority: 4 },
-  { id: 5, task: "Task-5", status: "todo", priority: 5 },
+  { id: 1, title: "Task-1", status: "todo", priority: 1 },
+  { id: 2, title: "Task-2", status: "progress", priority: 2 },
+  { id: 3, title: "Task-3", status: "review", priority: 3 },
+  { id: 4, title: "Task-4", status: "done", priority: 4 },
+  { id: 5, title: "Task-5", status: "todo", priority: 5 },
 ];
 const taskStatuses = ["todo", "progress", "review", "done"];
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
   return (
-    <div className="App">
-      <Column tasks={tasks} />
+    <div className="container">
+      <div className="row">
+        {taskStatuses.map((status) => (
+          <Column
+            key={status}
+            tasks={tasks}
+            status={status}
+            statuses={taskStatuses}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Column.js
+++ src/Column.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 const Column = (props) => {
+  console.log(props.tasks);
   return (
-    <div className="column">
-      {props.tasks.map((task) => (
-        <div className="card mb-3" key={task.id}>
-          <div className="card-body">
-            <h5 className="card-title">{task.title}</h5>
-            <p>{task.status}</p>
-            <p>{task.priority}</p>
+    <div className="col">
+      <h3>{props.status}</h3>
+      {props.tasks
+        .filter((task) => task.status === props.status)
+        .map((task) => (
+          <div className="card mb-4" key={task.id}>
+            <div className="card-body">
+              <h5 className="card-title">{task.title}</h5>
+              <p>{task.status}</p>
+              <p>{task.priority}</p>
+              <select
+                className="form-select"
+                aria-label="Default select example"
+                defaultValue={props.status}
+              >
+                {props.statuses.map((status) => (
+                  <option key={status} value={status}>
+                    {status}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
